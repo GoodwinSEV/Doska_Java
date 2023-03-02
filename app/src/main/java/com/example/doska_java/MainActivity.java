5cfd874ad914113e710959006d9fe437a5d129d6
GoodwinSEV <esavvina51@gmail.com>
2023-03-02 13:10:04 +0900

    Initial commit

---
+++ app/src/main/java/com/example/doska_java/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.doska_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private tvTextView TextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View
+
+
+    }
+}
